4c283f27d133f4abb083499a413b0bd5d740f9bd
SukhdeepSingh1234 <ss6540029@gmail.com>
2023-08-21 16:16:48 +0530

    instagram forntend done

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+          {/* Add the updated favicon link */}
+          <link rel="icon" href="/favicon.ico" />
+        </head>
       <body className={inter.className}>{children}</body>
+
     </html>
   )
 }
